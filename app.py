2b2035d32a238699f80e0752e45fb5ca313c9318
Alfreelancenow <alekseyfreelancenow@gmail.com>
2024-10-17 23:57:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from flask import Flask, request, render_template
-import requests  # Для отправки HTTP-запросов
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# URL твоего Google Apps Script веб-приложения
-api_url = 'https://script.google.com/macros/s/AKfycbzxrff5VxixVOKMRSDBi-a8FcwBiiYyabT905YthURJ2zIBKS8UcySiu-5lEnweja0o/exec'
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/buy')
+@app.route('/buy', methods=['GET', 'POST'])
 def buy():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        # Здесь должна быть логика для проверки наличия продукта
+        return f'Вы запросили продукт: {product_name}'  # Просто для теста
     return render_template('buy.html')
-
-@app.route('/check_product', methods=['POST'])
-def check_product():
-    product_name = request.form['product']  # Получаем название продукта от пользователя
-
-    # Отправляем запрос на Google Apps Script и получаем ответ
-    response = requests.get(api_url, params={'product': product_name})
-    
-    # Извлекаем текстовый ответ от Google Apps Script
-    message = response.text
-    
-    return render_template('result.html', message=message)  # Отображаем результат на странице
 
 if __name__ == '__main__':
     app.run(debug=True)
